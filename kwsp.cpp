5fb36d6911cede492a0154f1910c83ab6b461272
Muhammad Najmi Ahmad Zabidi <raden@users.noreply.github.com>
2019-02-16 00:47:08 +0800

    Init file

---
+++ kwsp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int tahun;
+float asas, untung, untung_tahun, jumlah_untung, untung_berompok;
+
+cout<<"Masukkan jumlah asas pelaburan"<<endl;
+cin>>asas;
+
+cout<<"Masukkan jumlah tahun pelaburan"<<endl;
+cin>>tahun;
+
+cout<<"Masukkan kadar untung setahun"<<endl;
+cin>>untung;
+
+untung_berompok=0;
+
+for (int i=0;i<tahun;i++){
+untung_tahun=((untung_berompok+asas)*(untung/100));
+untung_berompok+=untung_tahun;
+jumlah_untung=asas+untung_berompok;
+cout<<"Untung tahun "<<i+1<<" ialah RM "<<untung_tahun<<endl;
+}
+
+cout<<"Jumlah baru dana ialah RM "<<jumlah_untung<<endl;
+}
+
+
